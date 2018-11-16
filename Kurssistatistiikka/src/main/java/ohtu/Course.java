9293f138c717c9125c6b6640cb725c6cef0aa660
Lauri Väänänen <lauri.e.helsinki.fi>
2018-11-16 20:30:22 +0200

    finished

--- Kurssistatistiikka/src/main/java/ohtu/Course.java
+++ Kurssistatistiikka/src/main/java/ohtu/Course.java
@@ -16,6 +16,9 @@ public class Course {
     public boolean miniproject;
     public List<Integer> exercises;
     private List<Submission> submissions = new ArrayList<Submission>();
+    private int totalStudents;
+    private int totalSubmissions;
+    private int totalHours;
 
     public Course(){
 
@@ -31,6 +34,48 @@ public class Course {
 
     public List<Submission> getSubmissions() {
         return this.submissions;
+    }
+
+    /**
+     * @param totalHours the totalHours to set
+     */
+    public void setTotalHours(int totalHours) {
+        this.totalHours = totalHours;
+    }
+
+    /**
+     * @param totalStudents the totalStudents to set
+     */
+    public void setTotalStudents(int totalStudents) {
+        this.totalStudents = totalStudents;
+    }
+
+    /**
+     * @param totalSubmissions the totalSubmissions to set
+     */
+    public void setTotalSubmissions(int totalSubmissions) {
+        this.totalSubmissions = totalSubmissions;
+    }
+
+    /**
+     * @return the totalHours
+     */
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    /**
+     * @return the totalStudents
+     */
+    public int getTotalStudents() {
+        return totalStudents;
+    }
+
+    /**
+     * @return the totalSubmissions
+     */
+    public int getTotalSubmissions() {
+        return totalSubmissions;
     }
 
     @Override
--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,6 +1,8 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
 import org.apache.http.client.fluent.Request;
 import java.util.Arrays;
@@ -12,7 +14,7 @@ import java.util.stream.IntStream;;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String studentNr = "88";
+        String studentNr = "8";
 
         if ( args.length > 0) {
             studentNr = args[0];
@@ -22,18 +24,50 @@ public class Main {
 
         String coursesUrl = "https://studies.cs.helsinki.fi/courses/courseinfo";
 
+        String ohtuUrl = "https://studies.cs.helsinki.fi/courses/ohtu2018/stats";
+
+        String railsUrl = "https://studies.cs.helsinki.fi/courses/rails2018/stats";
+
         String bodyText = Request.Get(url).execute().returnContent().asString();
         String coursesBodyText = Request.Get(coursesUrl).execute().returnContent().asString();
-
-        // System.out.println("json-muotoinen data:");
-        // System.out.println(bodyText);
+        String ohtuText = Request.Get(ohtuUrl).execute().returnContent().asString();
+        String railsText = Request.Get(railsUrl).execute().returnContent().asString();
 
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         Course[] courses = mapper.fromJson(coursesBodyText, Course[].class);
+        JsonParser parser = new JsonParser();
+        JsonObject ohtuData = parser.parse(ohtuText).getAsJsonObject();
+        JsonObject railsData = parser.parse(railsText).getAsJsonObject();
 
         for (int x = 0; x < courses.length; x++) {
             Course course = courses[x];
+
+            JsonObject dataSet = null;
+            int students = 0;
+            int hour_total = 0;
+            int exercise_total = 0;
+
+            if (course.name.equals("ohtu2018")) {
+                dataSet = ohtuData;
+                for (String key : dataSet.keySet()) {
+                    students += dataSet.getAsJsonObject(key).get("students").getAsInt();
+                    hour_total += dataSet.getAsJsonObject(key).get("hour_total").getAsInt();
+                    exercise_total += dataSet.getAsJsonObject(key).get("exercise_total").getAsInt();
+                }
+            } else if (course.name.equals("rails2018")) {
+                dataSet = railsData;
+                for (String key : dataSet.keySet()) {
+                    students += dataSet.getAsJsonObject(key).get("students").getAsInt();
+                    hour_total += dataSet.getAsJsonObject(key).get("hour_total").getAsInt();
+                    exercise_total += dataSet.getAsJsonObject(key).get("exercise_total").getAsInt();
+                }
+            }
+
+            course.setTotalHours(hour_total);
+            course.setTotalStudents(students);
+            course.setTotalSubmissions(exercise_total);
+
             for (int y = 0; y < subs.length; y++) {
                 if ( subs[y].getCourse().equals(course.name )) {
                     Submission sub = subs[y];
@@ -59,6 +93,17 @@ public class Main {
             sb.append(" tehtävää ");
             Stream<Submission> stream_2 = course.getSubmissions().stream();
             sb.append(stream_2.mapToInt(x -> x.getHours()).sum());
+            sb.append(" tuntia");
+            System.out.println(sb.toString());
+
+            sb = new StringBuilder();
+            sb.append("\n");
+            sb.append("kursilla yhteensä ");
+            sb.append(course.getTotalStudents());
+            sb.append(" palautusta, palautettuja tehtäviä ");
+            sb.append(course.getTotalSubmissions());
+            sb.append(" kpl, aikaa käytetty yhteensä ");
+            sb.append(course.getTotalHours());
             sb.append(" tuntia");
             System.out.println(sb.toString());
             }
